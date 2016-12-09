19053b5f7db526ed89a6149353234f869f10f904
Pranav Chikodi <pranav.chikodi@mindtree.com>
2016-12-08 19:44:47 -0500

    Added reference for Heroku deployment

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ app.get('/', function(req,res){
 	res.sendFile(__dirname +'/index.html');
 });
 
-var port = 3000;
+//var port = 3000;
+var port = process.env.PORT || 8080;
 
 var server = app.listen(port, function(req,res){
 	console.log("Server is listening at ",port);
